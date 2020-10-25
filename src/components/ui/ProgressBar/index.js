f17c375aa6fefe4629ef8fbd0286a645ddc0f687
dnuanes <41339752+dnuanes@users.noreply.github.com>
2020-10-24 20:14:12 -0700

    remove progress bar 2

--- src/components/ui/ProgressBar/index.js
+++ src/components/ui/ProgressBar/index.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 import * as Styled from './styles';
 
-const ProgressBar = ({ title, percentage }) => (
-  <Styled.ProgressBar>
-    <Styled.Content>
-      <Styled.Title>{title}</Styled.Title>
-      <Styled.Percentage>{percentage}%</Styled.Percentage>
-    </Styled.Content>
-    <Styled.BarWrapper>
-      <Styled.Bar percentage={percentage} />
-    </Styled.BarWrapper>
-  </Styled.ProgressBar>
-);
+// const ProgressBar = ({ title, percentage }) => (
+//   <Styled.ProgressBar>
+//     <Styled.Content>
+//       <Styled.Title>{title}</Styled.Title>
+//       <Styled.Percentage>{percentage}%</Styled.Percentage>
+//     </Styled.Content>
+//     <Styled.BarWrapper>
+//       <Styled.Bar percentage={percentage} />
+//     </Styled.BarWrapper>
+//   </Styled.ProgressBar>
+// );
 
 ProgressBar.propTypes = {
   title: PropTypes.string.isRequired,

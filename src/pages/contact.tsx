ea272082adba70b91416e5fe6419e021c1ff4fb0
dnuanes <41339752+dnuanes@users.noreply.github.com>
2020-11-29 16:29:18 -0800

    addded banner to page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
+import ContactBanner from 'components/ContactBanner';
 import ContactInfo from 'components/ContactInfo';
 
 const ContactPage: React.FC = () => {
   return (
     <Layout>
       <SEO title="Contact" />
+      <ContactBanner />
       <ContactInfo />
     </Layout>
   );
